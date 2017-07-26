e2093d727ad21c56d28869e7416ff01484fb06fe
Leslie Zhai <lesliezhai@llvm.org.cn>
2017-07-26 13:46:52 +0800

    Only for GCC v6.3 to generate gt-cache.inc #21

--- src/Cache.cpp
+++ src/Cache.cpp
@@ -46,9 +46,7 @@ extern "C" {
 #include "coretypes.h"
 #include "tm.h"
 #include "tree.h"
-#if (GCC_MAJOR > 4)
 #include "tree-core.h"
-#endif
 
 #include "ggc.h"
 #ifndef ENABLE_BUILD_WITH_CXX
@@ -68,16 +66,7 @@ struct GTY(()) tree2int {
 #define tree2int_hash tree_map_base_hash
 #define tree2int_marked_p tree_map_base_marked_p
 
-#if (GCC_MAJOR < 5)
-// FIXME: gengtype does not support macro https://gcc.gnu.org/ml/gcc/2017-07/msg00061.html
-static GTY((if_marked("tree2int_marked_p"), param_is(struct tree2int)))
-    htab_t intCache;
-#else
-#if (GCC_MAJOR == 5)
-struct intCacheHasher : ggc_cache_hasher<tree2int *> {
-#else
 struct intCacheHasher : ggc_cache_ptr_hash<tree2int> {
-#endif
   static inline hashval_t hash(tree2int *t2i) {
     return tree_map_base_hash(&t2i->base);
   }
@@ -87,7 +76,6 @@ struct intCacheHasher : ggc_cache_ptr_hash<tree2int> {
   }
 };
 static GTY((cache)) hash_table<intCacheHasher> *intCache;
-#endif
 
 // Hash table mapping trees to Type*.
 
@@ -108,16 +96,7 @@ struct GTY(()) tree2Type {
 #define tree2Type_hash tree_map_base_hash
 #define tree2Type_marked_p tree_map_base_marked_p
 
-#if (GCC_MAJOR < 5)
-// FIXME: gengtype does not support macro https://gcc.gnu.org/ml/gcc/2017-07/msg00061.html
-static GTY((if_marked("tree2Type_marked_p"), param_is(struct tree2Type)))
-    htab_t TypeCache;
-#else
-#if (GCC_MAJOR == 5)
-struct TypeCacheHaser : ggc_cache_hasher<tree2Type *> {
-#else
 struct TypeCacheHaser : ggc_cache_ptr_hash<tree2Type> {
-#endif
   static inline hashval_t hash(tree2Type *t2T) {
     return tree_map_base_hash(&t2T->base);
   }
@@ -127,7 +106,6 @@ struct TypeCacheHaser : ggc_cache_ptr_hash<tree2Type> {
   }
 };
 static GTY((cache)) hash_table<TypeCacheHaser> *TypeCache;
-#endif
 
 // Hash table mapping trees to WeakVH.
 
@@ -148,16 +126,7 @@ struct GTY(()) tree2WeakVH {
 #define tree2WeakVH_hash tree_map_base_hash
 #define tree2WeakVH_marked_p tree_map_base_marked_p
 
-#if (GCC_MAJOR < 5)
-// FIXME: gengtype does not support macro https://gcc.gnu.org/ml/gcc/2017-07/msg00061.html
-static GTY((if_marked("tree2WeakVH_marked_p"), param_is(struct tree2WeakVH)))
-    htab_t WeakVHCache;
-#else
-#if (GCC_MAJOR == 5)
-struct WeakVHCacheHasher : ggc_cache_hasher<tree2WeakVH *> {
-#else
 struct WeakVHCacheHasher : ggc_cache_ptr_hash<tree2WeakVH> {
-#endif
   static inline hashval_t hash(tree2WeakVH *t2W) {
     return tree_map_base_hash(&t2W->base);
   }
@@ -171,23 +140,12 @@ struct WeakVHCacheHasher : ggc_cache_ptr_hash<tree2WeakVH> {
   }
 };
 static GTY((cache)) hash_table<WeakVHCacheHasher> *WeakVHCache;
-#endif
 
 // Include the garbage collector header.
 #ifndef ENABLE_BUILD_WITH_CXX
 extern "C" {
 #endif
-#if (GCC_MAJOR < 5)
-#if GCC_VERSION_CODE > GCC_VERSION(4, 5)
-#include "dragonegg/gt-cache-4.6.inc"
-#else
-#include "dragonegg/gt-cache-4.5.inc"
-#endif
-#else
-#if (GCC_MAJOR == 6)
 #include "dragonegg/gt-cache-6.3.inc"
-#endif
-#endif
 #ifndef ENABLE_BUILD_WITH_CXX
 } // extern "C"
 #endif
@@ -195,14 +153,9 @@ extern "C" {
 bool getCachedInteger(tree t, int &Val) {
   if (!intCache)
     return false;
-#if (GCC_MAJOR < 5)
-  tree_map_base in = { t };
-  tree2int *h = (tree2int *)htab_find(intCache, &in);
-#else
   tree2int in;
   in.base.from = t;
   tree2int *h = intCache->find(&in);
-#endif
   if (!h)
     return false;
   Val = h->val;
@@ -211,33 +164,12 @@ bool getCachedInteger(tree t, int &Val) {
 
 void setCachedInteger(tree t, int Val) {
   if (!intCache)
-#if (GCC_MAJOR < 5)
-    intCache = htab_create_ggc(1024, tree2int_hash, tree2int_eq, 0);
-#else
     intCache = hash_table<intCacheHasher>::create_ggc(1024);
-#endif
 
-#if (GCC_MAJOR < 5)
-  tree_map_base in = { t };
-  tree2int **slot = (tree2int **)htab_find_slot(intCache, &in, INSERT);
-#else
   tree2int in;
   in.base.from = t;
   tree2int **slot = intCache->find_slot(&in, INSERT);
-#endif
   assert(slot && "Failed to create hash table slot!");
-
-  if (!*slot) {
-#if (GCC_MAJOR < 5)
-    *slot =
-#if GCC_VERSION_CODE > GCC_VERSION(4, 5)
-        ggc_alloc_tree2int();
-#else
-    GGC_NEW(struct tree2int);
-#endif
-    (*slot)->base.from = t;
-#endif
-  }
 
   (*slot)->val = Val;
 }
@@ -245,61 +177,28 @@ void setCachedInteger(tree t, int Val) {
 Type *getCachedType(tree t) {
   if (!TypeCache)
     return 0;
-#if (GCC_MAJOR < 5)
-  tree_map_base in = { t };
-  tree2Type *h = (tree2Type *)htab_find(TypeCache, &in);
-#else
   tree2Type in;
   in.base.from = t;
   tree2Type *h = TypeCache->find(&in);
-#endif
   return h ? h->Ty : 0;
 }
 
 void setCachedType(tree t, Type *Ty) {
-#if (GCC_MAJOR < 5)
-  tree_map_base in = { t };
-#else
   tree2Type in;
   in.base.from = t;
-#endif
 
   /* If deleting, remove the slot.  */
   if (!Ty) {
     if (TypeCache)
-#if (GCC_MAJOR < 5)
-      htab_remove_elt(TypeCache, &in);
-#else
       TypeCache->remove_elt(&in);
-#endif
     return;
   }
 
   if (!TypeCache)
-#if (GCC_MAJOR < 5)
-    TypeCache = htab_create_ggc(1024, tree2Type_hash, tree2Type_eq, 0);
-#else
     TypeCache = hash_table<TypeCacheHaser>::create_ggc(1024);
-#endif
 
-#if (GCC_MAJOR < 5)
-  tree2Type **slot = (tree2Type **)htab_find_slot(TypeCache, &in, INSERT);
-#else
   tree2Type **slot = TypeCache->find_slot(&in, INSERT);
-#endif
   assert(slot && "Failed to create hash table slot!");
-
-#if (GCC_MAJOR < 5)
-  if (!*slot) {
-    *slot =
-#if GCC_VERSION_CODE > GCC_VERSION(4, 5)
-        ggc_alloc_tree2Type();
-#else
-    GGC_NEW(struct tree2Type);
-#endif
-    (*slot)->base.from = t;
-  }
-#endif
 
   (*slot)->Ty = Ty;
 }
@@ -309,14 +208,9 @@ void setCachedType(tree t, Type *Ty) {
 Value *getCachedValue(tree t) {
   if (!WeakVHCache)
     return 0;
-#if (GCC_MAJOR < 5)
-  tree_map_base in = { t };
-  tree2WeakVH *h = (tree2WeakVH *)htab_find(WeakVHCache, &in);
-#else
   tree2WeakVH in;
   in.base.from = t;
   tree2WeakVH *h = WeakVHCache->find(&in);
-#endif
   return h ? h->V : 0;
 }
 
@@ -328,54 +222,25 @@ static void DestructWeakVH(void *p) {
 /// given GCC tree.  The association is removed if tree is garbage collected
 /// or the value deleted.
 void setCachedValue(tree t, Value *V) {
-#if (GCC_MAJOR < 5)
-  tree_map_base in = { t };
-#else
   tree2WeakVH in;
   in.base.from = t;
-#endif
 
   // If deleting, remove the slot.
   if (!V) {
     if (WeakVHCache)
-#if (GCC_MAJOR < 5)
-      htab_remove_elt(WeakVHCache, &in);
-#else
       WeakVHCache->remove_elt(&in);
-#endif
     return;
   }
 
   if (!WeakVHCache)
     WeakVHCache =
-#if (GCC_MAJOR < 5)
-        htab_create_ggc(1024, tree2WeakVH_hash, tree2WeakVH_eq, DestructWeakVH);
-#else
         hash_table<WeakVHCacheHasher>::create_ggc(1024);
-#endif
 
-#if (GCC_MAJOR < 5)
-  tree2WeakVH **slot = (tree2WeakVH **)htab_find_slot(WeakVHCache, &in, INSERT);
-#else
   tree2WeakVH **slot = WeakVHCache->find_slot(&in, INSERT);
-#endif
   assert(slot && "Failed to create hash table slot!");
 
   if (*slot) {
     (*slot)->V = V;
     return;
   }
-
-#if (GCC_MAJOR < 5)
-  *slot =
-#if GCC_VERSION_CODE > GCC_VERSION(4, 5)
-      ggc_alloc_tree2WeakVH();
-#else
-  GGC_NEW(struct tree2WeakVH);
-#endif
-  (*slot)->base.from = t;
-  WeakVH *W = new (&(*slot)->V) WeakVH(V);
-  assert(W == &(*slot)->V && "Pointer was displaced!");
-  (void)W;
-#endif
 }
