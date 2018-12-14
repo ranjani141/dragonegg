dae83625807422ce34b960e09a88ccba6accc4bc
ranjani141 <45766204+ranjani141@users.noreply.github.com>
2018-12-14 21:39:59 +0530

    Add files via upload

---
+++ Examples/test4.adb
@@ -0,0 +1,14 @@
+package body test4 is
+
+c : short_integer;
+A : F32Array1by3;
+
+procedure test41 is
+
+begin
+   c := 67;
+   A(1) := 5.7e3;
+   
+end test41;
+	
+end test4;	
---
+++ Examples/test4.ads
@@ -0,0 +1,10 @@
+with obclib;
+Use  obclib;
+
+package test4 is
+
+Type F32Array1by3 is array (short_integer range 1..3) of float32;
+
+procedure test41;
+                      
+end test4;
