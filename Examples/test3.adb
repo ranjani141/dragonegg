2623bf9c41965317a02857ecb3a34f4bda6d54f8
ranjani141 <ranjani141@gmail.com>
2018-12-15 18:27:26 +0530

    Example updated

--- Examples/test3.adb
+++ Examples/test3.adb
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-package body test3 is
-a,b : float32;
-c : short_integer;
-
-procedure test31 is
-
-begin
-   c := 67;
-   a := float32(c);
-end test31;
-	
-end test3;	
-=======
 package body test3 is
 a,b : float32;
 c : short_integer;
@@ -24,4 +10,3 @@ begin
 end test31;
 	
 end test3;
->>>>>>> refs/remotes/origin/master
