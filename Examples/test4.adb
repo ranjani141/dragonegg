9c6d62878e04867435ec4a6efd82d72f796d7f76
ranjani141 <ranjani141@gmail.com>
2019-04-17 10:56:23 +0530

    Test program modified

---
+++ Examples/test4.adb
@@ -0,0 +1,17 @@
+package body test4 is
+
+--c : short_integer;
+--A : F32Array1by3;
+
+procedure test41 is
+
+c : short_integer;
+A : F32Array1by3;
+
+begin
+   c := 67;
+   --A(1) := 5.7e3;
+   
+end test41;
+	
+end test4;	
