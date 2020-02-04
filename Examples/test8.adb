1a6b414a8faddc6a67523ba8fefd16285a87efa6
ranjani141 <ranjani141@gmail.com>
2020-02-04 21:51:16 +0530

    Test8 added

---
+++ Examples/test8.adb
@@ -0,0 +1,8 @@
+package body test8 is
+
+procedure FirstInit is
+begin
+	BRate(1) := 0.0;
+end FirstInit;
+
+end test8;
---
+++ Examples/test8.ads
@@ -0,0 +1,9 @@
+with math;
+
+package test8 is
+
+   BRate : Math.F32Array1by3;
+
+   procedure FirstInit;
+
+end test8;
